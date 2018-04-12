12dc83b40926fa9d554762ddc6a874f76ea15fe5
Matthew Lee <matt@kynelee.com>
2018-04-12 01:28:23 -0400

    ccxt api integration

--- engine/market_poller.py
+++ engine/market_poller.py
@@ -1,19 +1,38 @@
-import endpoint_wrapper
+# coding=utf-8
+import ccxt
+import time
+import pprint
 
+delay = 2  # seconds
 
-class MarketPoller:
-    # order_book = {exchange_name: crypto_name: {bid: named_tuple(bid_price, bid_quantity), ask: named_tuple(ask_price, ask_quantity)}}
-    def __init__(self, config=None):
-        self.exchanges = config["exchanges"]
-        self.order_book = {}
+pp = pprint.PrettyPrinter(indent=2)
 
-    # Populate the order book with prices from all exchanges
-    def get_market_data(self):
-        for exchange in self.exchanges:
-            prices = endpoint_wrapper.get_exchange_prices(exchange)
-            print("Prices for Exchange: {}".format(exchange))
-            print(prices)
+binance = ccxt.binance()
+binance_markets = binance.load_markets()
 
+gdax = ccxt.gdax()
+gdax_markets = gdax.load_markets()
 
-x = MarketPoller(config={"exchanges": ["GDAX"]})
-x.get_market_data()
+# pp.pprint(gdax.fetch_order_book("ETH/BTC"))
+# pp.pprint(binance.fetch_order_book("ETH/BTC"))
+while True:
+
+    gdax_order_book = gdax.fetch_order_book("ETH/BTC")
+    binance_order_book = binance.fetch_order_book("ETH/BTC")
+
+    gdax_sell_price = gdax_order_book['bids'][0][0]
+    gdax_buy_price = gdax_order_book['asks'][0][0]
+
+    binance_sell_price = binance_order_book['bids'][0][0]
+    binance_buy_price = binance_order_book['asks'][0][0]
+
+    print(binance_sell_price)
+    print(binance_buy_price)
+
+    print("Profit to sell ETH on gdax, buy on binance: Price Difference {}, Percent: {}".format(
+        binance_buy_price-gdax_sell_price, ((binance_buy_price-gdax_sell_price)/float(gdax_sell_price)) * 100))
+    print("Profit to sell ETH on binance, buy on gdax: Price Difference: {}, Percent: {}".format(
+        gdax_buy_price - binance_sell_price, ((gdax_buy_price - binance_sell_price)/float(binance_sell_price)) * 100))
+    print("--------------------------------")
+
+    time.sleep(2)
