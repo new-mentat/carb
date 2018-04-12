c7853a9c5e29f25a824b29be09fa731e50bb1f2e
Matthew Lee <matt@kynelee.com>
2018-04-11 21:49:20 -0400

    separate market polling and arbitrage identification

--- engine/market_poller.py
+++ engine/market_poller.py
@@ -1,12 +1,10 @@
 import endpoint_wrapper
 
 
-class ArbitrageIdentifier:
+class MarketPoller:
     # order_book = {exchange_name: crypto_name: {bid: named_tuple(bid_price, bid_quantity), ask: named_tuple(ask_price, ask_quantity)}}
     def __init__(self, config=None):
         self.exchanges = config["exchanges"]
-        #self.constraints = config["constraints"]
-        #self.cryptos = config["cryptos"]
         self.order_book = {}
 
     # Populate the order book with prices from all exchanges
@@ -16,14 +14,6 @@ class ArbitrageIdentifier:
             print("Prices for Exchange: {}".format(exchange))
             print(prices)
 
-    # Identify arbitrage opportunities within a single exchange, subject to constraints
-    def identify_single_market_arbitrage(self):
-        pass
 
-    # Identify arbitrage opportunities across exchanges, subject to constraints
-    def identify_multi_market_arbitrage(self):
-        pass
-
-
-x = ArbitrageIdentifier(config={"exchanges": ["GDAX"]})
+x = MarketPoller(config={"exchanges": ["GDAX"]})
 x.get_market_data()
